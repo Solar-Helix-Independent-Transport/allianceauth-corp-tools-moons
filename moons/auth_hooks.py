ef8b2184a88195010923c2f68f1c255a3766062c
Aaron Kable <aaronkable@gmail.com>
2022-04-17 19:00:07 +0800

    Fix up perms new version

--- moons/auth_hooks.py
+++ moons/auth_hooks.py
@@ -4,20 +4,6 @@ from allianceauth import hooks
 from . import urls
 
 
-class MoonsHook(MenuItemHook):
-    def __init__(self):
-        MenuItemHook.__init__(self,
-                              _('Moon Board'),
-                              'fas fa-moon fa-fw',
-                              'moons:list',
-                              navactive=['moons:'])
-
-    def render(self, request):
-        if request.user.is_superuser:
-            return MenuItemHook.render(self, request)
-        return ''
-
-
 class MoonsBetaHook(MenuItemHook):
     def __init__(self):
         MenuItemHook.__init__(self,
@@ -27,14 +13,12 @@ class MoonsBetaHook(MenuItemHook):
                               navactive=['moons:r'])
 
     def render(self, request):
-        if request.user.has_perm('moons.view_available'):
+        if (request.user.has_perm('moons.view_available') or
+            request.user.has_perm('moons.view_corp') or
+            request.user.has_perm('moons.view_alliance') or
+                request.user.has_perm('moons.view_all')):
             return MenuItemHook.render(self, request)
         return ''
-
-
-@hooks.register('menu_item_hook')
-def register_menu():
-    return MoonsHook()
 
 
 @hooks.register('menu_item_hook')
