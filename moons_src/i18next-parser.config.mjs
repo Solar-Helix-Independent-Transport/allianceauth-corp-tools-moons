af5c7f4d7ae0003cf64633db960753e775277449
Joel Falknau <joel.falknau@gmail.com>
2024-08-31 20:29:43 +1000

    add translation config to react

---
+++ moons_src/i18next-parser.config.mjs
@@ -0,0 +1,10 @@
+export default {
+  locales: ["en", "fr"],
+  output: "locales/$LOCALE.$NAMESPACE.json",
+  namespaceSeparator: "::",
+  resetDefaultValueLocale: "en",
+  defaultValue: (locale, namespace, key) => key,
+  input: [
+    "src/**/*.{js,jsx,ts,tsx}",
+  ],
+};
