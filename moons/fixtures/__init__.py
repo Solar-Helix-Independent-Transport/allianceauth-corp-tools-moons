22ea8df6dca222d25644fa89faa420130ac3662b
Aaron Kable <aaronkable@gmail.com>
2026-03-04 19:36:10 +0800

    update the fixtures to match upstream

--- moons/fixtures/__init__.py
+++ moons/fixtures/__init__.py
@@ -5,7 +5,7 @@ try:
     types_to_load = [46319, 46309, 46301, 46295, 46285]
     testdata_spec: list[ModelSpec] = [
         ModelSpec("ItemType", ids=types_to_load),
-        ModelSpec("ItemTypeMaterials", ids=types_to_load, key = "item_type_id"),
+        ModelSpec("ItemTypeMaterials", ids=types_to_load, field="item_type_id"),
     ]
 except ImportError:
     pass
